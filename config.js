ea9174a615820aea06e2b0844d4c55da6d7a0bf7
S-Kittiwat <68015483+S-Kittiwat@users.noreply.github.com>
2026-08-19 15:17:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //  แก้ไขที่นี่ที่เดียว มีผลทุกหน้า
 // ============================================================
 var DOCSCAN_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwZbMi9CCxbcB54T-Qj-n1_1mzSiBgquI3xOX3py9-0DkI05JeEOOp3VtrqxoIwFY3erw/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyh_esb5bKIT57-9hwwlyHXerwHixZxmZaS21qwcUo2n03n2Db0TMPPOFzPEOk55trQOA/exec',
   APP_NAME: 'DocScan',
   VERSION: '2.0.1'
 };
