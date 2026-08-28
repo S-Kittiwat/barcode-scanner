f57bc27512c218f40ed444ce24dc74380cd078b3
S-Kittiwat <68015483+S-Kittiwat@users.noreply.github.com>
2026-08-28 10:33:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@
 //  ทั้งที่ Deploy ฝั่งเซิร์ฟเวอร์ไปแล้ว — หาสาเหตุยากมาก
 // ============================================================
 var DOCSCAN_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzp_W5pNvI84GK4mFu6NM1CWEnJLUuE-Q6-AyJP8Py2apn3-4ZUcoRnXZ1BSrZq1w0kyw/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzPUMnSaQKw_DB7xyCEiR0EEsrJXZG4-t3Kzed9EV_uUM44Ic_S71rel0RmqLeBbsBKQg/exec',
   APP_NAME: 'DocScan',
-  VERSION: '2.15.2'
+  VERSION: '2.15.4'
 };
 
 
