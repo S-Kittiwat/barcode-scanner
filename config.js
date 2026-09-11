70f55d81b21e85923fbbab6e371f376e2f71b8e5
S-Kittiwat <68015483+S-Kittiwat@users.noreply.github.com>
2026-09-11 12:12:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@
 //  ถ้าต้องการให้เห็นทันที ให้กด Ctrl+Shift+R
 // ============================================================
 var DOCSCAN_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwLfHGXShWE2XfXV60yH4vpEi3KHvqo6WhdVBRRErtobdhCgmiVP8-1D9dLdg0J7Ix8AA/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwvq1yKFKPxqsR_viku3HW38gtnX-qmee5ifSdarsnrOdT6ymeg0Iw6GfM2N2HCAd2gFQ/exec',
   APP_NAME: 'DocScan',
   VERSION: '2.15.7'
 };
